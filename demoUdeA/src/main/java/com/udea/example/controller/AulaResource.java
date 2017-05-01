00504a5d171676175d5b0df590f5ced57870c8ce
Anderson Mejia <ander1298@gmail.com>
2017-05-01 00:45:21 -0500

    Added schema creation from sql file and added aula resource api rest

--- demoUdeA/src/main/java/com/udea/example/controller/AulaResource.java
+++ demoUdeA/src/main/java/com/udea/example/controller/AulaResource.java
@@ -1,36 +1,132 @@
 package com.udea.example.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.udea.example.controller.util.HeaderUtil;
 import com.udea.example.entity.Aula;
 import com.udea.example.repository.AulaRepository;
 
-/**	
+/**
  * REST controller for managing Aula.
  */
 @Controller
 @RequestMapping("/api")
 public class AulaResource {
 
-    private final Logger log = LoggerFactory.getLogger(AulaResource.class);
+	private final Logger log = LoggerFactory.getLogger(AulaResource.class);
 
-    @Autowired
+	private static final String ENTITY_NAME = "aula";
+
+	@Autowired
 	private AulaRepository aulaRepository;
-    
-	@GetMapping("aulas")
-	public ResponseEntity<List<Aula>> getAulas() {
-	    List<Aula> aulas = aulaRepository.findAll();
-	    return new ResponseEntity<List<Aula>>(aulas, HttpStatus.OK);
+
+	/**
+	 * POST /aulas : Create a new aula.
+	 *
+	 * @param aula
+	 *            the aula to create
+	 * @return the ResponseEntity with status 201 (Created) and with body the
+	 *         new aula, or with status 400 (Bad Request) if the aula has
+	 *         already an ID
+	 * @throws URISyntaxException
+	 *             if the Location URI syntax is incorrect
+	 */
+	// @PostMapping("/aulas")
+	@RequestMapping(value = "/aulas", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Aula> createAula(@RequestBody Aula aula) throws URISyntaxException {
+		log.debug("REST request to save Aula : {}", aula);
+		if (aula.getId() != null) {
+			return ResponseEntity.badRequest().headers(
+					HeaderUtil.createFailureAlert(ENTITY_NAME, "idexists", "A new aula cannot already have an ID"))
+					.body(null);
+		}
+		Aula result = aulaRepository.save(aula);
+		return ResponseEntity.created(new URI("/api/aulas/" + result.getId()))
+				.headers(HeaderUtil.createEntityCreationAlert(ENTITY_NAME, result.getId().toString())).body(result);
 	}
 
+	/**
+	 * PUT /aulas : Updates an existing aula.
+	 *
+	 * @param aula
+	 *            the aula to update
+	 * @return the ResponseEntity with status 200 (OK) and with body the updated
+	 *         aula, or with status 400 (Bad Request) if the aula is not valid,
+	 *         or with status 500 (Internal Server Error) if the aula couldnt be
+	 *         updated
+	 * @throws URISyntaxException
+	 *             if the Location URI syntax is incorrect
+	 */
+	@PutMapping("/aulas")
+	public ResponseEntity<Aula> updateAula(@RequestBody Aula aula) throws URISyntaxException {
+		log.debug("REST request to update Aula : {}", aula);
+		if (aula.getId() == null) {
+			return createAula(aula);
+		}
+		Aula result = aulaRepository.save(aula);
+		return ResponseEntity.ok().headers(HeaderUtil.createEntityUpdateAlert(ENTITY_NAME, aula.getId().toString()))
+				.body(result);
+	}
+
+	/**
+	 * GET /aulas : get all the aulas.
+	 *
+	 * @param filter
+	 *            the filter of the request
+	 * @return the ResponseEntity with status 200 (OK) and the list of aulas in
+	 *         body
+	 */
+	// @GetMapping("aulas")
+	@RequestMapping(value = "/aulas", method = RequestMethod.GET)
+	public ResponseEntity<List<Aula>> getAulas() {
+		List<Aula> aulas = aulaRepository.findAll();
+		return new ResponseEntity<List<Aula>>(aulas, HttpStatus.OK);
+	}
+
+	/**
+	 * GET /aulas/:id : get the "id" aula.
+	 *
+	 * @param id
+	 *            the id of the aula to retrieve
+	 * @return the ResponseEntity with status 200 (OK) and with body the aula,
+	 *         or with status 404 (Not Found)
+	 */
+	@GetMapping("/aulas/{id}")
+	public ResponseEntity<Aula> getAula(@PathVariable Long id) {
+		log.debug("REST request to get Aula : {}", id);
+		Aula aula = aulaRepository.findOne(id);
+		return new ResponseEntity<>(aula, HttpStatus.OK);
+	}
+
+	/**
+	 * DELETE /aulas/:id : delete the "id" aula.
+	 *
+	 * @param id
+	 *            the id of the aula to delete
+	 * @return the ResponseEntity with status 200 (OK)
+	 */
+	@DeleteMapping("/aulas/{id}")
+	public ResponseEntity<Void> deleteAula(@PathVariable Long id) {
+		log.debug("REST request to delete Aula : {}", id);
+		aulaRepository.delete(id);
+		return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert(ENTITY_NAME, id.toString())).build();
+	}
 
 }
---
+++ demoUdeA/src/main/resources/universidad.sql
@@ -0,0 +1,136 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         5.7.18-log - MySQL Community Server (GPL)
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Volcando estructura de base de datos para universidad
+CREATE DATABASE IF NOT EXISTS `universidad` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `universidad`;
+
+-- Volcando estructura para tabla universidad.aula
+CREATE TABLE IF NOT EXISTS `aula` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo_aula` varchar(255) DEFAULT NULL,
+  `ubicacion` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla universidad.aula: ~4 rows (aproximadamente)
+/*!40000 ALTER TABLE `aula` DISABLE KEYS */;
+INSERT INTO `aula` (`id`, `tipo_aula`, `ubicacion`) VALUES
+	(1, 'PRESENCIAL', 'BLOQUE ING SALA LIS'),
+	(2, 'PRESENCIAL', 'BLOQUE ING 19-201'),
+	(3, 'PRESENCIAL', 'BLOQUE ING 19-304'),
+	(4, 'PRESENCIAL', 'BLOQUE ING 21-216'),
+	(5, 'VIRTUAL', 'PLATAFORMA WIZIQ');
+/*!40000 ALTER TABLE `aula` ENABLE KEYS */;
+
+-- Volcando estructura para tabla universidad.estudiante
+CREATE TABLE IF NOT EXISTS `estudiante` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombres` varchar(255) DEFAULT NULL,
+  `apellidos` varchar(255) DEFAULT NULL,
+  `carrera` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla universidad.estudiante: ~5 rows (aproximadamente)
+/*!40000 ALTER TABLE `estudiante` DISABLE KEYS */;
+INSERT INTO `estudiante` (`id`, `nombres`, `apellidos`, `carrera`) VALUES
+	(3, 'Ana', 'Agudelo', 'ING SISTEMAS'),
+	(5, 'Harold', 'Castañeda', 'ING SISTEMAS'),
+	(6, 'Juan', 'Andrade', 'ING SISTEMAS'),
+	(7, 'Yeison', 'Jimenez', 'ING SISTEMAS'),
+	(8, 'Alexander', 'Uribe', 'ING SISTEMAS');
+/*!40000 ALTER TABLE `estudiante` ENABLE KEYS */;
+
+-- Volcando estructura para tabla universidad.estudiante_materia
+CREATE TABLE IF NOT EXISTS `estudiante_materia` (
+  `estudiante_id` int(11) NOT NULL,
+  `materia_id` int(11) NOT NULL,
+  PRIMARY KEY (`estudiante_id`,`materia_id`),
+  KEY `fk_materia_estudiante` (`materia_id`),
+  CONSTRAINT `fk_estudiante_materia` FOREIGN KEY (`estudiante_id`) REFERENCES `estudiante` (`id`),
+  CONSTRAINT `fk_materia_estudiante` FOREIGN KEY (`materia_id`) REFERENCES `materia` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla universidad.estudiante_materia: ~7 rows (aproximadamente)
+/*!40000 ALTER TABLE `estudiante_materia` DISABLE KEYS */;
+INSERT INTO `estudiante_materia` (`estudiante_id`, `materia_id`) VALUES
+	(3, 1),
+	(5, 1),
+	(8, 1),
+	(3, 2),
+	(5, 2),
+	(6, 3),
+	(7, 4);
+/*!40000 ALTER TABLE `estudiante_materia` ENABLE KEYS */;
+
+-- Volcando estructura para tabla universidad.materia
+CREATE TABLE IF NOT EXISTS `materia` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) DEFAULT NULL,
+  `hora_inicio` time DEFAULT NULL,
+  `hora_fin` time DEFAULT NULL,
+  `dias` varchar(50) DEFAULT NULL,
+  `aula_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_materia_aula_id` (`aula_id`),
+  CONSTRAINT `fk_materia_aula_id` FOREIGN KEY (`aula_id`) REFERENCES `aula` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla universidad.materia: ~4 rows (aproximadamente)
+/*!40000 ALTER TABLE `materia` DISABLE KEYS */;
+INSERT INTO `materia` (`id`, `nombre`, `hora_inicio`, `hora_fin`, `dias`, `aula_id`) VALUES
+	(1, 'ARQ DE SOFTWARE', '12:00:00', '14:00:00', 'MIERCOLES', 5),
+	(2, 'ARQ DE SOFTWARE', '10:00:00', '12:00:00', 'SABADOS', 5),
+	(3, 'BASE DE DATOS', '14:00:00', '16:00:00', 'LUNES', 1),
+	(4, 'BASE DE DATOS', '14:00:00', '16:00:00', 'JUEVES', 1);
+/*!40000 ALTER TABLE `materia` ENABLE KEYS */;
+
+-- Volcando estructura para tabla universidad.profesor
+CREATE TABLE IF NOT EXISTS `profesor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombres` varchar(255) DEFAULT NULL,
+  `apellidos` varchar(255) DEFAULT NULL,
+  `carrera` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla universidad.profesor: ~2 rows (aproximadamente)
+/*!40000 ALTER TABLE `profesor` DISABLE KEYS */;
+INSERT INTO `profesor` (`id`, `nombres`, `apellidos`, `carrera`) VALUES
+	(1, 'Anderson', 'Mejia', 'ING SISTEMAS'),
+	(2, 'Diego', 'Botia', 'ING SISTEMAS');
+/*!40000 ALTER TABLE `profesor` ENABLE KEYS */;
+
+-- Volcando estructura para tabla universidad.profesor_materia
+CREATE TABLE IF NOT EXISTS `profesor_materia` (
+  `profesor_id` int(11) NOT NULL,
+  `materia_id` int(11) NOT NULL,
+  PRIMARY KEY (`profesor_id`,`materia_id`),
+  KEY `fk_materia_profesor` (`materia_id`),
+  CONSTRAINT `fk_materia_profesor` FOREIGN KEY (`materia_id`) REFERENCES `materia` (`id`),
+  CONSTRAINT `fk_profesor_materia` FOREIGN KEY (`profesor_id`) REFERENCES `profesor` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla universidad.profesor_materia: ~3 rows (aproximadamente)
+/*!40000 ALTER TABLE `profesor_materia` DISABLE KEYS */;
+INSERT INTO `profesor_materia` (`profesor_id`, `materia_id`) VALUES
+	(1, 1),
+	(1, 2),
+	(2, 3);
+/*!40000 ALTER TABLE `profesor_materia` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
